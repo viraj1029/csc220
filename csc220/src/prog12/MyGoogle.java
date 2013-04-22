2bc6847314a323adffe45d81df9e68c8d7501a70
jewban <jewban@gmail.com>
2013-04-22 20:19:14 +0000

    Working search function

--- csc220/src/prog12/MyGoogle.java
+++ csc220/src/prog12/MyGoogle.java
@@ -15,15 +15,6 @@ public class MyGoogle implements Google {
 	private Map<String, Integer> url2Index = new TreeMap<String, Integer>();
 
 	@Override
-	/**
-	 * @param browser A browser object to gather from
-	 * @param startingURLs a List of URLs to start gathering from
-	 * Gathers relevant information from a webpage
-	 * Indexes all of the words on that webpage
-	 * Indexes all of the links on that webpage
-	 * Updates the reference count for links on that webpage (max 1)
-	 * Updates urlIndexList with a list of each webpage on which a given word appears
-	 */
 	public void gather(Browser browser, List<String> startingURLs) {
 		// TODO Auto-generated method stub
 		Set<Integer> refSet = new HashSet<Integer>();
@@ -35,6 +26,8 @@ public class MyGoogle implements Google {
 		for (int i = 0; i < startingURLs.size(); i++) {
 			urlQueue.offer(startingURLs.get(i));
 		}
+		// for (String startURL : startingURLs)
+		// urlQueue.offer(startURL);
 		int count = 0;
 		while (!urlQueue.isEmpty() && count++ < 100) {
 			refSet.clear();
@@ -50,14 +43,12 @@ public class MyGoogle implements Google {
 					indexURL(item);
 				}
 				int index = url2Index.get(item);
-				if (refSet.contains(index) == false)
-				{					
+				if (refSet.contains(index) == false) {
 					int temp = refCounts.get(index);
 					temp++;
 					refCounts.set(index, temp);
 					refSet.add(index);
 				}
-				
 
 			}
 			for (int i = 0; i < words.size(); i++) {
@@ -68,9 +59,12 @@ public class MyGoogle implements Google {
 				}
 				int index = word2Index.get(item);
 				List<Integer> theList = urlIndexLists.get(index);
-				if (theList.size() < 1)
-					theList.add(url2Index.get(url));
-				if (theList.get(theList.size() - 1) != url2Index.get(url))
+				/*
+				 * if (theList.size() < 1) theList.add(url2Index.get(url));
+				 */
+				if (theList.isEmpty()
+						|| theList.get(theList.size() - 1) != url2Index
+								.get(url))
 					theList.add(url2Index.get(url));
 			}
 		}
@@ -118,7 +112,105 @@ public class MyGoogle implements Google {
 	@Override
 	public String[] search(List<String> keyWords, int numResults) {
 		// TODO Auto-generated method stub
-		return null;
+		// 1. search all webpages containing key words
+		// 2. rank the candidate webpages by a priority queue according to
+		// refcounts
+
+		Iterator<Integer>[] pageIndexIterators = (Iterator<Integer>[]) new Iterator[keyWords
+				.size()];
+		int[] currentPageIndices = new int[keyWords.size()];
+		for (int i = 0; i < keyWords.size(); i++) {
+			pageIndexIterators[i] = urlIndexLists.get(
+					word2Index.get(keyWords.get(i))).iterator();
+			currentPageIndices[i] = urlIndexLists.get(
+					word2Index.get(keyWords.get(i))).get(0);
+		}
+		// current page index in each list, just "behind" the iterator
+		Stack<String> temp = new Stack<String>();
+		Comparator comparator = new URLComparator();
+		PriorityQueue<Integer> bestPageIndices = new PriorityQueue<Integer>(10,
+				comparator);
+		while (updateSmallest(currentPageIndices, pageIndexIterators)) {
+			if (allEqual(currentPageIndices))
+				bestPageIndices.offer(currentPageIndices[0]);
+		}
+		while (!bestPageIndices.isEmpty())
+			temp.push(allURLs.get(/* url2Index.get( */bestPageIndices.poll()/* ) */));
+		String[] ret = new String[temp.size()];
+		int i = 0;
+		while (!temp.isEmpty()) {
+			ret[i] = temp.pop();
+			i++;
+		}
+		return ret;
 	}
 
+	/**
+	 * Look through currentPageIndices for all the smallest elements. For each
+	 * smallest element currentPageIndices[i], load the next element from
+	 * pageIndexIterators[i]. If pageIndexIterators[i] does not have a next
+	 * element, return false. Otherwise, return true.
+	 * 
+	 * @param currentPageIndices
+	 *            array of current page indices
+	 * @param pageIndexIterators
+	 *            array of iterators with next page indices
+	 * @return true if all minimum page indices updates, false otherwise
+	 */
+	private boolean updateSmallest(int[] currentPageIndices,
+			Iterator<Integer>[] pageIndexIterators) {
+		int smallest = Integer.MAX_VALUE;
+		int smallestIndex = -1;
+			
+		for (int i = 0; i < currentPageIndices.length - 1; i++) {
+			if (!pageIndexIterators[i].hasNext())
+				return false;
+			if (currentPageIndices[i] < currentPageIndices[i + 1])
+			{
+				smallest = currentPageIndices[i];
+				smallestIndex = i;
+			}else if (currentPageIndices[i] > currentPageIndices[i+1]){
+				smallest = currentPageIndices[i + 1];
+				smallestIndex = i+1;
+			}
+		}
+		for (int k = 0; k < currentPageIndices.length; k++) {
+			if (currentPageIndices[k] <= smallest || smallestIndex == -1)
+				currentPageIndices[k] = pageIndexIterators[k].next();
+		}
+		return true;
+
+	}
+
+	/**
+	 * Check if all elements in an array are equal.
+	 * 
+	 * @param array
+	 *            an array of numbers
+	 * @return true if all are equal, false otherwise
+	 */
+	private boolean allEqual(int[] array) {
+		int result = 0;
+		for (int i = 0; i < array.length - 1; i++) {
+			if (array[i] != array[i + 1])
+				result++;
+		}
+		return result == 0;
+	}
+
+	public class URLComparator implements Comparator<Integer> {
+
+		@Override
+		public int compare(Integer o1, Integer o2) {
+			int ref1 = refCounts.get(o1);
+			int ref2 = refCounts.get(o2);
+			if (ref1 > ref2)
+				return 1;
+			if (ref1 < ref2)
+				return -1;
+			else
+				return 0;
+		}
+
+	}
 }
